3abb0979c737cd676808c29d89faa8904d7d579a
feaouill <ukrdoomstats@gmail.com>
2019-04-29 20:26:51 +0300

    begin

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1556558500,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/button-sheet0.png",
+		"images/ramka-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/war-sheet0.png",
+		"media/222922__noisecollector__smallgongcracked.m4a",
+		"media/222922__noisecollector__smallgongcracked.ogg",
+		"media/117118__unclesigmund__siren-low.m4a",
+		"media/117118__unclesigmund__siren-low.ogg",
+		"media/456071__sheriff__chinatown-funeral.m4a",
+		"media/456071__sheriff__chinatown-funeral.ogg",
+		"media/regtime.m4a",
+		"media/regtime.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
